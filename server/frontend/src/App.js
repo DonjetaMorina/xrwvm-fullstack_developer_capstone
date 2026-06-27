824507b8586ac13827b0a12543279be4a30f6545
donjeta morina <donjeta.morina9@student.uni-pr.edu>
2026-06-27 11:47:37 -0400

    feat: implement user registration functionality and verified app workflows

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,11 +1,15 @@
-import LoginPanel from "./components/Login/Login"
+import LoginPanel from "./components/Login/Login";
+import Register from "./components/Register/Register"; // 1. Import your new component
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
+      {/* 2. Add the route path for registration */}
+      <Route path="/register" element={<Register />} /> 
     </Routes>
   );
 }
+
 export default App;
