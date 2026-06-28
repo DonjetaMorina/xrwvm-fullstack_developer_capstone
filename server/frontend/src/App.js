a636388c410acd9f92858024ee33561254a799c8
DonjetaMorina <donjeta.morina9@student.uni-pr.edu>
2026-06-27 20:20:50 -0400

    done with Add Dynamic Pages

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,6 +1,11 @@
 import LoginPanel from "./components/Login/Login";
 import Register from "./components/Register/Register"; // 1. Import your new component
 import { Routes, Route } from "react-router-dom";
+import Dealers from './components/Dealers/Dealers';
+import Dealer from "./components/Dealers/Dealer"
+import PostReview from "./components/Dealers/PostReview"
+
+
 
 function App() {
   return (
@@ -8,6 +13,10 @@ function App() {
       <Route path="/login" element={<LoginPanel />} />
       {/* 2. Add the route path for registration */}
       <Route path="/register" element={<Register />} /> 
+      <Route path="/dealers" element={<Dealers/>} />
+      <Route path="/dealer/:id" element={<Dealer />} />
+      <Route path="/postreview/:id" element={<PostReview />} />
+      
     </Routes>
   );
 }
